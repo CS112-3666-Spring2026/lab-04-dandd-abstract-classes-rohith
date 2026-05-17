ecca25b195dab87f1431d1fab09fae6044ba0156
rmekala3623 <rmekala3623@student.miracosta.edu>
2026-05-17 23:35:25 +0000

    Finished lab by adding dwarf.java and modifying elf.java and Main.java

--- Dwarf.java
+++ Dwarf.java
@@ -1,30 +1,30 @@
-public class Elf extends GameCharacter {
+public class Dwarf extends GameCharacter{
     //CONSTANT VARIABLES
-    public static final String DEFAULT_NAME = "Legolas", DEFAULT_CLASS_TYPE = "Druid", DEFAULT_ALIGNMENT = "Neutral";
+    public static final String DEFAULT_NAME = "Gimli", DEFAULT_CLASS_TYPE = "Cleric", DEFAULT_ALIGNMENT = "Neutral";
     public static final int DEFAULT_GOLD = 1000000, DEFAULT_EXP_POINTS = 1, DEFAULT_HIT_POINTS = 1, DEFAULT_ARMOR_CLASS = 1;
-    public static final Weapon DEFAULT_WEAPON1 = new Weapon("Crossbow", 6, 30, 3), DEFAULT_WEAPON2 = new Weapon();
+    public static final Weapon DEFAULT_WEAPON1 = new Weapon("Hammer", 6, 1, 4), DEFAULT_WEAPON2 = new Weapon();
 
     //CONSTRUCTORS
-    public Elf(String name, String classType, String alignment, int gold, int expPoints,
+    public Dwarf(String name, String classType, String alignment, int gold, int expPoints,
             int hitPoint, int armorClass, Weapon weapon1, Weapon weapon2) {
             super(name, classType, alignment, gold, expPoints, hitPoint, armorClass, weapon1, weapon2);
     }
 
-    public Elf() {
-        this(Elf.DEFAULT_NAME, Elf.DEFAULT_CLASS_TYPE, Elf.DEFAULT_ALIGNMENT, Elf.DEFAULT_GOLD, Elf.DEFAULT_EXP_POINTS,
-                Elf.DEFAULT_HIT_POINTS, Elf.DEFAULT_ARMOR_CLASS, Elf.DEFAULT_WEAPON1, Elf.DEFAULT_WEAPON2);
+    public Dwarf() {
+        this(Dwarf.DEFAULT_NAME, Dwarf.DEFAULT_CLASS_TYPE, Dwarf.DEFAULT_ALIGNMENT, Dwarf.DEFAULT_GOLD, Dwarf.DEFAULT_EXP_POINTS,
+                Dwarf.DEFAULT_HIT_POINTS, Dwarf.DEFAULT_ARMOR_CLASS, Dwarf.DEFAULT_WEAPON1, Dwarf.DEFAULT_WEAPON2);
     }
 
-    public Elf(Elf original) {
+    public Dwarf(Dwarf original) {
         super(original);
     }
 
     @Override
     public void assist(GameCharacter other) {
-        if (other instanceof Elf) {
-            int healed = other.getHitPoint() + 10;
-            other.setHitPoint(healed);
-            System.out.println("Healed ally by 10 points!");
+        if (other instanceof Dwarf) {
+            int armorUp = other.getArmorClass() + 10;
+            other.setArmorClass(armorUp);
+            System.out.println("Increased ally's armor class by 10 points!");
         } else {
             int goldUpdated = other.getGold() + 5;
             other.setGold(goldUpdated);
@@ -34,7 +34,7 @@ public class Elf extends GameCharacter {
 
     @Override
     public boolean attack(GameCharacter other) {
-        int attackDamage = this.getExpPoints() / other.getArmorClass();
+        int attackDamage = this.getExpPoints() + this.getWeapon1().getDamage();
         int updatedHitPoints = other.getHitPoint();
 
         if (attackDamage >= updatedHitPoints) {
@@ -44,7 +44,7 @@ public class Elf extends GameCharacter {
         } else {
             updatedHitPoints = updatedHitPoints - attackDamage;
             other.setHitPoint(updatedHitPoints);
-            System.out.println("Did " + attackDamage + " points of damage.");
+            System.out.println(this.getName() + " did " + attackDamage + " points of damage to " + other.getName() + ".");
             return false; //function returns false if opponent survived the attack.
         }
     }
--- Main.java
+++ Main.java
@@ -1,2 +1,12 @@
-//Main.java
+public class Main {
+    public static void main(String[] args) {
+        Dwarf bruenor = new Dwarf("Bruenor", "Barbarian", "Neutral", 1000000, 1, 10, 10, new Weapon("Axe", 3, 1, 4),
+                null);
+        System.out.println(bruenor);
 
+        Dwarf gimli = new Dwarf();
+        System.out.println(gimli);
+
+        gimli.attack(bruenor);
+    }
+}
